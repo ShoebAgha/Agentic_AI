b016ca6a18bccf26b7c700078807c0b60a1e25b3
ShoebAgha <shoebagha@gmail.com>
2026-07-02 08:23:40 +0530

    Structured Agent reponse

--- main.py
+++ main.py
@@ -6,19 +6,29 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_anthropic import ChatAnthropic
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
 
 tavily = TavilySearch()
 
+class Sources(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description='The url of the source')
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer:str = Field(description="The agent's answer to the query")
+    sources: list[Sources] = Field(description="List of sources used to generate reponse to the query")
 
 llm = ChatAnthropic(model = "claude-haiku-4-5-20251001")
 tools=[tavily]
 
-agent=create_agent(model=llm, tools=tools)
+agent=create_agent(model=llm, tools=tools,response_format=AgentResponse)
 
 
 def main():
     print('hello from langchain')
-    result=agent.invoke({'messages':HumanMessage('What is the weather in tokyo?')})
+    result=agent.invoke({'messages':HumanMessage('Find 3 active AI Engineer role in Delhi NCR along with pay')})
     print(result)
 
 if __name__ == '__main__':
